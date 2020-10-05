e5884d3dc01a99dad58cdb05d5ce85b0e5d2228a
Wilmar A. Stephens <wilmars@alumni.brown.edu>
2020-10-05 00:24:44 -0400

    questions and inqurer added

--- index.js
+++ index.js
@@ -1,5 +1,47 @@
+const inquirer = require("inquirer");
+
+require("inquirer");
+
+
 // array of questions for user
 const questions = [
+
+    {
+        type: "input",
+        name: "userName",
+        message: "What is your name?",
+    },
+    {
+        type: "input",
+        name: "eMail",
+        message: "What is your e-mail?",
+        validate: function (value) {
+            const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+            return re.test(String(value).toLowerCase()) || "Please enter a properly formatted e-mail";
+        }
+    },
+    {
+        type: "input",
+        name: "githubUsername",
+        message: "What is your GitHub Username?"
+    },
+    {
+        type: "input",
+        name: "projectTitle",
+        message: "What is the title of the project?",
+    },
+    {
+        type: "input",
+        name: "projectDescription",
+        message: "Project desscription:"
+    },
+    {
+        type: "input",
+        name: "projectLicense",
+        message: "Project License",
+    },
+
+
 
 ];
 
@@ -9,7 +51,10 @@ function writeToFile(fileName, data) {
 
 // function to initialize program
 function init() {
-
+    inquirer.prompt(questions)
+        .then((answers) => {
+            console.log(answers);
+        })
 }
 
 // function call to initialize program
