38b5530923de6976e20bc0c288ccbe41623a8cce
Wilmar A. Stephens <wilmars@alumni.brown.edu>
2020-10-07 23:54:57 -0400

    added template

---
+++ build-readme.js
@@ -0,0 +1,41 @@
+const readmeTemplate = `
+# ${projectTitle}
+
+## Description
+${projectDescription}
+## Table of Contents
+- [Install](#install)
+- [Usage](#usage)
+- [Tests](#tests)
+- [Questions](#questions)
+- [Contributing](#contributing)
+- [License](#license)
+
+## Install
+
+${installInstructions}
+
+## Usage
+
+${usageInformation}
+
+## Tests
+
+${testInformation}
+
+## Questions
+
+Contact:
+
+[${githubUsername}](https://github.com/${githubUsername}) on GitHub
+
+[e-mail](mailto:${eMail})
+
+## Contributing
+
+${contributionGuidelines}
+
+## License
+
+${projectLicense}
+`
