356b04131876951fd1574dfcd11b40486a2f840d
Wilmar A. Stephens <wilmars@alumni.brown.edu>
2020-10-07 23:37:15 -0400

    moved license choice array

--- license-utils.js
+++ license-utils.js
@@ -64,5 +64,68 @@ GithubLicense.prototype.getGitHubAPI = async function (endpoint) {
 // const ghLicense = new GithubLicense();
 
 // ghLicense.getLicenseList();
+const featuredLicenseList =
+    [
+        {
+            name: 'GNU Affero General Public License v3.0',
+            value: 'agpl-3.0',
+            short: 'GNU Affero General Public License v3.0'
+        },
+        {
+            name: 'Apache License 2.0',
+            value: 'apache-2.0',
+            short: 'Apache License 2.0'
+        },
+        {
+            name: 'BSD 2-Clause "Simplified" License',
+            value: 'bsd-2-clause',
+            short: 'BSD 2-Clause "Simplified" License'
+        },
+        {
+            name: 'BSD 3-Clause "New" or "Revised" License',
+            value: 'bsd-3-clause',
+            short: 'BSD 3-Clause "New" or "Revised" License'
+        },
+        {
+            name: 'Boost Software License 1.0',
+            value: 'bsl-1.0',
+            short: 'Boost Software License 1.0'
+        },
+        {
+            name: 'Creative Commons Zero v1.0 Universal',
+            value: 'cc0-1.0',
+            short: 'Creative Commons Zero v1.0 Universal'
+        },
+        {
+            name: 'Eclipse Public License 2.0',
+            value: 'epl-2.0',
+            short: 'Eclipse Public License 2.0'
+        },
+        {
+            name: 'GNU General Public License v2.0',
+            value: 'gpl-2.0',
+            short: 'GNU General Public License v2.0'
+        },
+        {
+            name: 'GNU General Public License v3.0',
+            value: 'gpl-3.0',
+            short: 'GNU General Public License v3.0'
+        },
+        {
+            name: 'GNU Lesser General Public License v2.1',
+            value: 'lgpl-2.1',
+            short: 'GNU Lesser General Public License v2.1'
+        },
+        { name: 'MIT License', value: 'mit', short: 'MIT License' },
+        {
+            name: 'Mozilla Public License 2.0',
+            value: 'mpl-2.0',
+            short: 'Mozilla Public License 2.0'
+        },
+        { name: 'The Unlicense', value: 'unlicense', short: 'The Unlicense' }
+    ]
 
-module.exports = GithubLicense;
+module.exports = {
+    featuredLicenseList: featuredLicenseList,
+    GithubLicense,
+}
